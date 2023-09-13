faa26e3ccadfd0dc7cd26c19fcf4e56cf76309b1
Bach <motirelia612@gmail.com>
2023-09-13 16:06:37 +0700

    taskInitProject

---
+++ MoviesDB/MoviesDB/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  MoviesDB
+//
+//  Created by Bach Nghiem on 13/09/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+}
---
+++ MoviesDB/MoviesDBTests/MoviesDBTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesDBTests.swift
+//  MoviesDBTests
+//
+//  Created by Bach Nghiem on 13/09/2023.
+//
+
+import XCTest
+@testable import MoviesDB
+
+class MoviesDBTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
