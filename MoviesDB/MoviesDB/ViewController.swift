3f405810bad9c6dcee11b03f80ece23fb05320d0
Bach <motirelia612@gmail.com>
2023-09-13 16:13:28 +0700

    init project with swiftlint

--- MoviesDB/MoviesDB/ViewController.swift
+++ MoviesDB/MoviesDB/ViewController.swift
@@ -11,6 +11,6 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the .
     }
 }
